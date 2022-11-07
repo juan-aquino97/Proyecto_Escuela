fa87c6d0e718c8753b23949ac654535239836253
Juan Aquino <juancruza97@gmail.com>
2022-11-07 19:29:12 -0300

    Inicio del proyecto

---
+++ appcoder/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField()
+
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField()
+    profesion = models.CharField(max_length=100)
+
+
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=100)
+    fecha_de_entrega = models.DateField()
+    entregado = models.BooleanField()
